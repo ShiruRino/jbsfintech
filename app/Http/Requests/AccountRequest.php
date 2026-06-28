1764850286f0671714674e27cfa1734ec3b1736d
ShiruRino <tigugg654@gmail.com>
2026-06-28 15:13:27 +0700

    feat: extends formrequest -> baserequest

--- app/Http/Requests/AccountRequest.php
+++ app/Http/Requests/AccountRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class AccountRequest extends FormRequest
+class AccountRequest extends BaseRequest
 {
     /**
      * Determine if the user is authorized to make this request.
